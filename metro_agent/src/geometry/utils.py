9dd6dacb623b45929106e01f8b1b71f537bafd8a
xavierzhouzss <125638423+xavierzhouzss@users.noreply.github.com>
2026-02-22 14:09:26 -0800

    Update utils.py
    update a zero length protection at geometric normalization

--- metro_agent/src/geometry/utils.py
+++ metro_agent/src/geometry/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from src.geometry.point import Point
 
+EPS = 1e-9
 
 def get_distance(p1: Point, p2: Point) -> float:
     result: float = np.sqrt((p1.left - p2.left) ** 2 + (p1.top - p2.top) ** 2)
@@ -11,4 +12,6 @@ def get_distance(p1: Point, p2: Point) -> float:
 def get_direction(p1: Point, p2: Point) -> Point:
     diff = p2 - p1
     diff_magnitude = get_distance(p1, p2)
+    if diff_magnitude < EPS:
+        return Point(0.0, 0.0)
     return Point(diff.left / diff_magnitude, diff.top / diff_magnitude)
